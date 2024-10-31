ffae8127ce64a25a21b6bf4b8f425c3741077ae5
GXCP <147403353+GXCP@users.noreply.github.com>
2024-10-31 12:06:54 -0300

    z

---
+++ main.js
@@ -0,0 +1,10 @@
+function criarCartao(pergunta, resposta){
+    let conteudo = document.getElementById("conteudo")
+    let cartao = document.createElement("article")
+    cartao.classList = "cartao"
+
+    cartao.innerHTML = 
+
+    conteudo.appendChild(cartao)
+}
+criarCartao(1,2)
